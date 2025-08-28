d2d26c1ec26debd7dc480675c5702a45a92e6f29
Dhruvin Parikh <29928916+dhruvinparikh@users.noreply.github.com>
2025-08-28 19:14:33 -0400

    Deploy RemoteHop and RemoteMintRedeemHop on Scroll and Aurora (#26)
    * (chore): deploy on scroll
    
    * (chore): deploy hops on aurora
    
    ---------
    
    Co-authored-by: Carter Carlson <pegahcarter@gmail.com>

---
+++ src/script/hop/Remote/DeployRemoteHopAurora.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import { BaseScript } from "frax-std/BaseScript.sol";
+import { console } from "frax-std/BaseScript.sol";
+import { RemoteHop } from "src/contracts/hop/RemoteHop.sol";
+import { RemoteMintRedeemHop } from "src/contracts/hop/RemoteMintRedeemHop.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { IExecutor, ISendLibrary, IDVN } from "./DeployRemoteHop.sol";
+
+// forge script src/script/hop/Remote/DeployRemoteHopAurora.sol --rpc-url https://mainnet.aurora.dev --legacy --broadcast --verify --verifier blockscout --verifier-url https://explorer.aurora.dev/api/
+contract DeployRemoteHopAurora is BaseScript {
+    address constant FRAXTAL_HOP = 0x2A2019b30C157dB6c1C01306b8025167dBe1803B;
+    address constant FRAXTAL_MINTREDEEM_HOP = 0x3e6a2cBaFD864e09e6DAb9Cf035a0AbEa32bc0BC;
+
+    address EXECUTOR;
+    address DVN;
+    address SEND_LIBRARY;
+
+    address frxUsdOft;
+    address sfrxUsdOft;
+    address frxEthOft;
+    address sfrxEthOft;
+    address wFraxOft;
+    address fpiOft;
+    address[] approvedOfts;
+
+    constructor() {
+        EXECUTOR = 0xA2b402FFE8dd7460a8b425644B6B9f50667f0A61;
+        DVN = 0xD4a903930f2c9085586cda0b11D9681EECb20D2f;
+        SEND_LIBRARY = 0x1aCe9DD1BC743aD036eF2D92Af42Ca70A1159df5;
+
+        frxUsdOft = 0x80Eede496655FB9047dd39d9f418d5483ED600df;
+        sfrxUsdOft = 0x5Bff88cA1442c2496f7E475E9e7786383Bc070c0;
+        frxEthOft = 0x43eDD7f3831b08FE70B7555ddD373C8bF65a9050;
+        sfrxEthOft = 0x3Ec3849C33291a9eF4c5dB86De593EB4A37fDe45;
+        wFraxOft = 0x64445f0aecC51E94aD52d8AC56b7190e764E561a;
+        fpiOft = 0x90581eCa9469D8D7F5D3B60f4715027aDFCf7927;
+    }
+
+    function run() public broadcaster {
+        _validateAddrs();
+
+        approvedOfts.push(frxUsdOft);
+        approvedOfts.push(sfrxUsdOft);
+        approvedOfts.push(frxEthOft);
+        approvedOfts.push(sfrxEthOft);
+        approvedOfts.push(wFraxOft);
+        approvedOfts.push(fpiOft);
+
+        RemoteHop remoteHop = new RemoteHop({
+            _fraxtalHop: bytes32(uint256(uint160(FRAXTAL_HOP))),
+            _numDVNs: 3,
+            _EXECUTOR: EXECUTOR,
+            _DVN: DVN,
+            _TREASURY: ISendLibrary(SEND_LIBRARY).treasury(),
+            _approvedOfts: approvedOfts
+        });
+        console.log("RemoteHop deployed at:", address(remoteHop));
+
+        RemoteMintRedeemHop remoteMintRedeemHop = new RemoteMintRedeemHop({
+            _fraxtalHop: bytes32(uint256(uint160(FRAXTAL_MINTREDEEM_HOP))),
+            _numDVNs: 3,
+            _EXECUTOR: EXECUTOR,
+            _DVN: DVN,
+            _TREASURY: ISendLibrary(SEND_LIBRARY).treasury(),
+            _EID: 30211,
+            _frxUsdOft: frxUsdOft,
+            _sfrxUsdOft: sfrxUsdOft
+        });
+        console.log("RemoteMintRedeemHop deployed at:", address(remoteMintRedeemHop));
+    }
+
+    function _validateAddrs() internal view returns (bool) {
+        (uint64 major, uint8 minor, uint8 endpointVersion) = ISendLibrary(SEND_LIBRARY).version();
+        require(major == 3 && minor == 0 && endpointVersion == 2, "Invalid SendLibrary version");
+
+        require(IExecutor(EXECUTOR).endpoint() != address(0), "Invalid executor endpoint");
+        require(IDVN(DVN).vid() != 0, "Invalid DVN vid");
+
+        require(isStringEqual(IERC20Metadata(frxUsdOft).symbol(), "frxUSD"), "frxUsdOft != frxUSD");
+        require(isStringEqual(IERC20Metadata(sfrxUsdOft).symbol(), "sfrxUSD"), "sfrxUsdOft != sfrxUSD");
+        require(isStringEqual(IERC20Metadata(frxEthOft).symbol(), "frxETH"), "frxEthOft != frxETH");
+        require(isStringEqual(IERC20Metadata(sfrxEthOft).symbol(), "sfrxETH"), "sfrxEthOft != sfrxETH");
+        require(isStringEqual(IERC20Metadata(wFraxOft).symbol(), "WFRAX"), "wFraxOft != WFRAX");
+        require(isStringEqual(IERC20Metadata(fpiOft).symbol(), "FPI"), "fpiOft != FPI");
+    }
+
+    function isStringEqual(string memory _a, string memory _b) public pure returns (bool) {
+        return keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b));
+    }
+}
